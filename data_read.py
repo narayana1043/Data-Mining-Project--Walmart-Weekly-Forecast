6b4e41b528b66cfaae842a4fcdc0ff003ea24132
veera.marni <narayana1043@gmail.com>
2016-03-16 03:34:03 -0400

    Please take a look into this if you are new to python

--- data_read.py
+++ data_read.py
@@ -9,9 +9,24 @@ Use the function naming rules: lowercase with words separated by underscores as 
 '''
 
 import pandas as pd
+import re
 
-def data_read(fileName):
+#Reading data from files to generate a pandas data frame
+def dataFrameGen(fileName):
     dataFrame = pd.read_csv(fileName, header = 0)
-    print(dataFrame.head())
+    #print(dataFrame.head())
+    return dataFrame
 
-data_read("stores.csv")
+#reading the files from locations and making a dictionary of data
+def read_data():
+    # data file names
+    fileNames = ["stores.csv","features.csv","train.csv"]
+    #creating a dictionary of data example data[stores] has data of the stores as dataframes defined by pandas
+    data = {re.sub(r".csv","",file) : dataFrameGen(file) for file in fileNames}
+    #displaying the first five lines of data from every file
+    #this is only to cross check that everything works as per thoughts
+    for key in data.keys():
+        print(data[key].head())
+
+
+read_data()
