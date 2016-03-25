ad223592e65bbbffb9752bfb90ae48bee687565a
veera.marni <narayana1043@gmail.com>
2016-03-25 07:54:56 -0400

    Figured out that temperature feature is only correlated for a very few departments and that too in only particular stores.
    We can say that is not correlated completely or we can also add a rule that whenever that particular department of that stores needs a prediction we can include temperature to impprove accuracy of the prediction
    
    I will also check the correlation with other features and let you know on that.

--- data_read.py
+++ data_read.py
@@ -15,6 +15,10 @@ import pandas as pd
 import re
 import os
 from datetime import datetime
+from sklearn import preprocessing
+min_max_scaler = preprocessing.MinMaxScaler()
+from matplotlib import pyplot as plt
+from scipy.stats import pearsonr
 
 dataPath = "data/"
 dataFileNames = ["stores.csv", "historical_features.csv", "future_features.csv", "train.csv"]
@@ -50,7 +54,8 @@ def dataFrameGen(fileName):
                 row['Date'].update(row['date'], value)
             except Exception:
                 pass
-        dataFrame["Date"] = dataFrame["Date"].map(dateToWeekNumMap)   #mapping the map to the dataframe to update
+        dataFrame["Date"] = dataFrame["Date"].map(dateToWeekNumMap)     #mapping the map to the dataframe to update
+        dataFrame.rename(columns = {"Date":"WeekNum"}, inplace = True)  #updating the column name
     return dataFrame
 
 #reading the files from locations and making a dictionary of data
@@ -60,13 +65,49 @@ def read_data() -> dict:
     data = {re.sub(r".csv","",file) : dataFrameGen(file) for file in dataFileNames}
     #displaying the first five lines of data from every file
     #this is only to cross check that everything works as per thoughts
-    for key,value in data.items():
-        print(key)
-        print(value.head(),"\n")
-
+    # for key,value in data.items():
+    #     print(key)
+    #     print(value.head(),"\n")
+    train = data['train']
+    train['Temperature'] = None
+    historical_features = data['historical_features']
+    #print(historical_features.head())
+    for index,row in historical_features.iterrows():
+        Store = row['Store']
+        WeekNum = row['WeekNum']
+        Temperature = row['Temperature']
+        train.Temperature[(train['Store'] == Store) & (train['WeekNum'] == WeekNum)] = Temperature
     return data
 
 #Example usage for pickling/unpickling
-# pickle_data(read_data())
-data = read_pickled_data()
+#pickle_data(read_data())
 
+def normalize():
+    #reading pickle data
+    data = read_pickled_data()
+    train = data['train']
+    print(train.head())
+    #min max normalization for temperatures
+    #train['Temperature'] = min_max_scaler.fit_transform(train['Temperature'])
+    #min max normalization for sales
+    #train['Weekly_Sales'] = min_max_scaler.fit_transform(train['Weekly_Sales'])
+    for storeNum in range(1,46):
+        for deptNum in range(1,100):
+            pearsonCorr = abs(pearsonr(train.Weekly_Sales[(train['Dept'] == storeNum) & (train['Store'] == deptNum)],(train.Temperature[(train['Dept'] == storeNum) & (train['Store'] == deptNum)]))[0])
+            if pearsonCorr > 0.8:
+                print(storeNum,",",deptNum,",",pearsonCorr)
+
+""" *************for ploting correaltion**************************
+    for store in range(1,46):
+        for dept in range(1,100):
+            x = train.Weekly_Sales[(train['Dept'] == dept) & (train['Store'] == store)]
+            y = train.Temperature[(train['Dept'] == dept) & (train['Store'] == store)]
+            plt.scatter(x,y)
+    plt.text(6, 1.5, 'Each data point denotes a department in a store for a particular date', style='italic',fontsize=10,bbox={'facecolor':'grey', 'alpha':0.5, 'pad':10})
+    plt.title('Scatter plot to figure out there exists a relation b/w sales and temperatures',fontsize=14)
+    plt.xlabel("Weekly Sales")
+    plt.ylabel("Temperatures")
+    plt.show()
+    *******************************************************************"""
+
+normalize()
